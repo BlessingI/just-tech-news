a6acaf5f98ce9b90f0418401ac0b81d425addbb8
Blessing <bigbadumhe@gmail.com>
2022-05-18 23:37:59 -0500

    lesson4

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Comment, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
+  console.log("======================");
   Post.findAll({
-    // update the `.findAll()` method's attributes to look like this
     attributes: [
       "id",
       "post_url",
@@ -21,6 +20,14 @@ router.get("/", (req, res) => {
     ],
     order: [["created_at", "DESC"]],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -52,6 +59,14 @@ router.get("/:id", (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -85,14 +100,13 @@ router.post("/", (req, res) => {
     });
 });
 
-// PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then((updatedPostData) => res.json(updatedPostData))
+  Post.upvote(req.body, { Vote, Comment, User })
+    .then((updatedVoteData) => res.json(updatedVoteData))
     .catch((err) => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
 });
 
@@ -138,4 +152,5 @@ router.delete("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
 module.exports = router;
